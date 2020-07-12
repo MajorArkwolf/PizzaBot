45c55d00999042d8675181d784f13deb6dae3763
Peter <petercrabbe92@gmail.com>
2020-07-12 23:05:24 +0800

    Add basic functionality

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pizza Bot
+### Get the Pizza before the boi's.
+#### Introduction
+Pizza is delicious and I wanted it before anyone else. A few members on our server post when a free pizza is available
+at the local pizza shop they work out. So this bot is designed to scrape our discord channel and notify me as soon
+as one becomes available.
+
+#### Getting Started
+Make sure you having the following installed
+
+```discord.py python-dotenv``` 
---
+++ main.py
@@ -0,0 +1,47 @@
+import discord
+import os
+from dotenv import load_dotenv
+from os.path import join, dirname
+
+client = discord.Client()
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# The trust channels to look for free pizza
+trusted_channel = [718113875203063899, 605754665732407316]
+# The trusted people who will bestow the free pizza
+trusted_people = [184291535221817348, 465038624170180609]
+# The bois to tell to claim said free pizza
+the_true_boy = 184291535221817348
+# The key words to look for when pizza is available
+key_words = ["pizza", "free", "BBQ", "chicken", "some like it hot", "hot", "supreme", "meat", "lovers", "lover", "Got a", "pepperoni", "family size", "hawaiian", "hawian"]
+
+
+async def notify_the_boi(message):
+    user = await client.fetch_user(the_true_boy)
+    send_message = discord.Embed(title="Pizza Alert")
+    send_message.add_field(name=f"User: {message.author}", value=f"{message.content}", inline=True)
+    await user.send(embed=send_message)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if any(message.channel.id == x for x in trusted_channel):
+        if any(message.author.id == x for x in trusted_people):
+            if any(x.lower() in message.content.lower() for x in key_words):
+                await notify_the_boi(message)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+TOKEN = os.environ.get("DISCORD_BOT_SECRET")
+client.run(TOKEN)
